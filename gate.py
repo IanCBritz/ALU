ea74fb9a7de3b9e9b8352d3cee47009c60024bc9
Ian Britz <iancbritz@gmail.com>
2017-02-08 19:40:07 +0800

    Add files via upload

--- gate.py
+++ gate.py
@@ -8,6 +8,91 @@
 #
 #Purpose: list of logic gates
 
+def MUX(s, a, b):
+    """""""""""""""""
+
+    Input: 0, 1
+    
+    Output: Selection between a and b
+
+    s a b | out
+    -----------
+    0 0 0 |  0
+    0 0 1 |  0
+    0 1 0 |  1
+    0 1 1 |  1
+    1 0 0 |  0
+    1 0 1 |  1
+    1 1 0 |  0
+    1 1 1 |  1
+    
+    Dependencies: NOT, AND, OR
+    
+    """""""""""""""""
+
+    return OR ( AND(a, NOT(s)), AND(b, s) )
+
+def XOR(a, b):
+    """""""""""""""""
+
+    Input: 0, 1
+    
+    Output: Logical Exclusive OR 
+
+    a b | out
+    ---------
+    0 0 |  0
+    0 1 |  1
+    1 0 |  1
+    1 1 |  0
+    
+    Dependencies: NAND, AND, OR
+    
+    """""""""""""""""
+
+    return AND ( NAND(a, b), OR(a,b) )
+    
+    
+def OR(a, b):
+    """""""""""""""""
+
+    Input: 0, 1
+    
+    Output: Logical OR
+
+    a b | out
+    ---------
+    0 0 |  0
+    0 1 |  1
+    1 0 |  1
+    1 1 |  1
+    
+    Dependencies: NAND
+    
+    """""""""""""""""
+
+    return NAND( NAND(a, '1'), NAND(b, '1') )
+
+    
+def AND(a, b):
+    """""""""""""""""
+
+    Input: 0, 1
+    
+    Output: Logical AND
+
+    a b | out
+    ---------
+    0 0 |  0
+    0 1 |  0
+    1 0 |  0
+    1 1 |  1
+    
+    Dependencies: NAND, NOT
+    
+    """""""""""""""""
+
+    return NOT( NAND(a, b) )
 
 def NOT(a):
     """""""""""""""""
@@ -41,13 +126,19 @@ def NAND(a, b):
     1 0 |  1
     1 1 |  0
     
-    Dependencies: NAND
+    Dependencies: None
     
     """""""""""""""""
 
     if a == '1' and b == '1':
+        
         return '0'
     else:
         return '1'
 
-print NOT('1')
+
+#Test case
+for i in xrange(0, 8):
+    b = list(format(i, '03b'))
+    print MUX(b[0], b[1], b[2])
+    
